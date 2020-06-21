a8b12d072d599c953fa7dfa2909f86aca6392fda
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-06-21 20:05:35 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,7 +5,12 @@ function basicTeenager(age) {
 }
 
 function teenager(age) {
-
+  if (age >= 13 && age <= 19){
+    return("You are a teenager!");
+  }
+  elseif{
+    
+  }
 }
 
 function ageChecker(age) {
