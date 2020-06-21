d12de08db976e7e4c533e7d52cc95d3d574029f8
James Fong <51034737+meiyaismywaifu@users.noreply.github.com>
2020-06-21 20:20:17 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -32,9 +32,9 @@ function ternaryTeenager(age) {
 }
 
 function switchAge(age) {
-  case (age >= 13 && age <= 19):
-    return("You are a teenager!");
-    break;
+  //case (age >= 13 && age <= 19):
+    //return("You are a teenager!");
+    //break;
   // default:
     // return("You have an age");
     // break
